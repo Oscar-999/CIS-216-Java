dfd5b4890d8587fdffa4a3d4927d9fc4218368e4
Quai <quai@Quais-MacBook-Air.local>
2025-01-27 21:44:05 -0600

    eod

--- Homeworks/Homework1/src/Car.java
+++ Homeworks/Homework1/src/Car.java
@@ -5,13 +5,14 @@ import java.util.Scanner;
 public class Car {
     public static void main(String[] args) {
         // Sets up the scanner for the user input
-        Scanner scanner = new Scanner(System.in);
-        // Variable declaration
         String name;
-        //Prompts user for car name
-        System.out.println("Type the name of a car:");
-        //saves userinput to the name variable
-        name = scanner.next();
+        try (Scanner scanner = new Scanner(System.in)) {
+            // Variable declaration
+            //Prompts user for car name
+            System.out.println("Type the name of a car:");
+            //saves userinput to the name variable
+            name = scanner.next();
+        }
         // Prints out the car name we typed
         System.out.printf("The car is a %s.\n", name);
 
